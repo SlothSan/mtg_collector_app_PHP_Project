ce0de04c920602c28d2075567f2e0a52bde4b69b
Mike Oram <Mikeoram01@gmail.com>
2022-08-01 15:14:19 +0100

    PHP Boilerplate and CSS normalize added.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="'Mike Oram', PHP, CSS, Magic The Gathering">
+        <meta name="description" content="Mike Oram's PHP Collectors App">
+        <link href="normalize.css" type="text/css" rel="stylesheet">
+        <link href="styles.css" type="text/css" rel="stylesheet">
+        <title>MTG Card Collector - PHP Project - iO Academy.</title>
+    </head>
+    <body>
+
+    </body>
+</html>
