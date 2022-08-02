4727516a2ebe8dc570fa3e1886684a1f0a437593
Mike Oram <Mikeoram01@gmail.com>
2022-08-02 18:20:10 +0100

    Testing file created and working - need to write the tests /cry.

---
+++ test/testFunctions.php
@@ -0,0 +1,10 @@
+<?php
+require '../functions.php';
+use PHPUnit\Framework\TestCase;
+class testFunctions extends TestCase {
+
+    public function testsortDatesSuccess()
+    {
+        echo 'Hello World!';
+    }
+}
